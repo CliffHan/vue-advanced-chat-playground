fbc245f051c601db8f3e0337edc9da95a34de385
Cliff <cliff.hp@gmail.com>
2024-05-28 21:40:03 +0800

    Import vue-advanced-chat

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: tagName => {
+            return tagName === 'vue-advanced-chat' || tagName === 'emoji-picker'
+          }
+        }
+      }
+    }),
   ],
   resolve: {
     alias: {
