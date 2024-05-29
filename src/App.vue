c46bc7094edad653cef688166f4e3e09d23e6fde
Cliff <cliff.hp@gmail.com>
2024-05-29 11:42:34 +0800

    Let add room and send message work

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <vue-advanced-chat
+  <vue-advanced-chat ref="chatComponent"
     :current-user-id="currentUserId"
+    :loading-rooms="loadingRooms"
+    :rooms-loaded="roomsLoaded"
+    :messages-loaded="messagesLoaded"
     .rooms="rooms"
     .messages="messages"
     .room-actions="roomActions"
+    @add-room="addRoom"
+    @fetch-messages="fetchMessages($event.detail[0])"
+    @send-message="sendMessage($event.detail[0])"
   />
 </template>
 
@@ -18,6 +24,9 @@ export default {
   data() {
     return {
       currentUserId: '1234',
+      loadingRooms: false,
+      roomsLoaded: true,
+      messagesLoaded: true,
       rooms: [],
       messages: [],
       roomActions: [
@@ -25,6 +34,51 @@ export default {
         { name: 'removeUser', title: 'Remove User' },
         { name: 'deleteRoom', title: 'Delete Room' }
       ]
+    }
+  },
+
+  mounted() {
+    window.chatWindow = this;
+  },
+
+  methods: {
+    addRoom() {
+      console.log('addRoom()');
+      let rooms = [...this.rooms];
+      rooms.push({
+        roomId: '1',
+        roomName: 'Room 1',
+        users: [
+          {
+            _id: '1234',
+            username: 'John Doe',
+          },
+          {
+            _id: '4321',
+            username: 'John Snow',
+          }
+        ],
+      });
+      this.rooms = rooms;
+    },
+
+    fetchMessages({ room, options }) {
+      console.log(`fetchMessages(), room=${JSON.stringify(room)}, options=${JSON.stringify(options)}`);
+      this.messagesLoaded = false;
+      // use timeout to imitate async server fetched data
+      setTimeout(() => {
+        this.messages = [];
+        this.messagesLoaded = true;
+      })
+    },
+
+    sendMessage(payload) {
+      console.log(`sendMessage(), payload=${JSON.stringify(payload)}`);
+      const messages = [...this.messages];
+      payload['_id'] = 1;
+      payload['senderId'] = '1234';
+      messages.push(payload);
+      this.messages = messages;
     }
   }
 }
